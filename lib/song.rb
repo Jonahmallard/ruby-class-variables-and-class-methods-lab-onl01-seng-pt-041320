05d4b7cff1925c1677ac39bebc3b10570f939857
Jonah Mallard <benjaminjmallard@gmail.com>
2020-05-10 00:35:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,26 +27,26 @@ class Song
   
   def self.genre_count
     genre_count = {}
-      @@genres.each do |genre|end
+      @@genres.each do |genre|
     if !genre_count[genre]
       genre_count[genre] = 1 
     else 
       genre_count[genre] += 1 
     end 
-  
+  end
   end 
   
   genre_count
 
   def self.artist_count
     artist_count = {}
-      @@artists.each do |artist|end
+      @@artists.each do |artist|
     if !artist_count[artist]
       artist_count[artist] = 1 
     else 
       artist_count[artist] += 1 
     end
-  
+  end
   end
   
 
